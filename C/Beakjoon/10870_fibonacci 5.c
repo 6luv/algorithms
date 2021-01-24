754a107d00735f45ade2c4a3663172d1c05ba478
jmj005 <jmj00005@naver.com>
2021-01-24 23:35:22 +0900

    add C files

---
+++ C/Beakjoon/10870_fibonacci 5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+	if (n == 0) //n이 0이면 0반환
+		return 0;
+	else if (n == 1 || n == 2) //n이 1이나 2이면 1반환
+		return 1;
+	else
+		return(fibonacci(n - 1) + fibonacci(n - 2)); 
+		// n이 2 이상이면 재귀함수로 값 구함
+}
+
+int main() {
+	int n;
+	scanf_s("%d", &n);
+	printf("%d", fibonacci(n)); //재귀함수 실행
+
+}
