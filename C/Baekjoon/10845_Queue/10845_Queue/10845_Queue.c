1128a41a3c44ad42228250a546fac6e7272aaec7
jmj005 <jmj00005@naver.com>
2021-02-10 23:43:00 +0900

    modify C file

--- C/Baekjoon/10845_Queue/10845_Queue/10845_Queue.c
+++ C/Baekjoon/10845_Queue/10845_Queue/10845_Queue.c
@@ -2,61 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-//스택 만들기
-#define MAX_SIZE 10001
-int stack[MAX_SIZE];
-int toop = -1;
-int count = 0;
-int i = -1;
+//큐 만들기
+#define MAX_SIZE 50000
+int queue[MAX_SIZE];
+int top = -1;							//맨 앞 가리킴
+int bottom = -1;						//맨 뒤 가리킴
+int count = 0;   
 
-//스택에 값 넣기
+//큐에 값 넣기
 void push(int x) {
-	//top을 1증가시킨 자리에 값 넣기
-	toop += 1;
-	stack[toop] = x;
-	count += 1;
+	if (top == -1) { 
+		top = 0;
+	}
+	queue[++bottom] = x;				//bottom을 1 증가시킴
+	count++;
 }
 
-//스택에 있는 값 출력
-void pop(void) {
-	//스택에 들어있는 정수가 없을 때
-	if (toop < 0) {
+//큐에 있는 값 출력
+void pop(void){
+	if (count == 0) {					//큐에 들어있는 정수가 없을 때
 		printf("-1\n");
-	}
-	//스택에 정수가 있으면 출력하고 top 1 감소
-	else {
-		i += 1;
-		printf("%d\n", stack[i]);
-		toop -= 1;
-		count -= 1;
-
+	} else {							//스택에 정수가 있으면 출력하고 top 1 감소
+		printf("%d\n", queue[top++]);	//맨 앞에 있는 큐 출력하고 1 증가
+		count--;
 	}
 }
 
-//스택에 들어있는 정수의 개수 출력
+//큐에 들어있는 정수의 개수 출력
 void size(void) {
 	printf("%d\n", count);
 }
 
-//스택이 비었는지 확인
+//큐가 비었는지 확인
 void empty(void) {
-	if (toop >= 0)	//스택이 비어있지 않음
+	if (count != 0)						//큐가 비어있지 않음
 		printf("0\n");
-	else			//스택이 비어있음
+	else								//큐가 비어있음
 		printf("1\n");
 }
 
 
-void front(void) {
-	if (toop >= 0)
-		printf("%d\n", stack[0]);
+void front(void) {						//맨 앞에 있는 큐 출력
+	if (count > 0)
+		printf("%d\n", queue[top]);		
 	else
 		printf("-1\n");
 }
 
-void back(void) {
-	if (toop >= 0)
-		printf("%d\n", stack[toop]);
+void back(void) {						//맨 뒤에 있는 큐 출력
+	if (count > 0)
+		printf("%d\n", queue[bottom]);
 	else
 		printf("-1\n");
 }
@@ -84,9 +79,8 @@ int main() {
 		else if (!strcmp(order, "front")) {
 			front();
 		}
-		else {
+		else if (!strcmp(order, "back")) {
 			back();
 		}
-
 	}
 }
