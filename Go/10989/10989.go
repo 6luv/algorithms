1437a1fe585822694451e518725c5a387fb1134b
jmj005 <jmj00005@naver.com>
2022-02-01 23:14:37 +0900

    add file

---
+++ Go/10989/10989.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+func main() {
+	var n int
+	r := bufio.NewReader(os.Stdin)
+	fmt.Fscan(r, &n)
+	var arr = make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &arr[i])
+	}
+
+	sort.Slice(arr, func(i, j int) bool {
+		return arr[i] < arr[j]
+	})
+
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	for i := 0; i < n; i++ {
+		fmt.Fprintln(w, arr[i])
+	}
+}
