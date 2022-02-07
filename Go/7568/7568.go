61972b28c599014068653155ce8169c3b0a166a2
jmj005 <jmj00005@naver.com>
2022-02-07 22:26:06 +0900

    add files

---
+++ Go/7568/7568.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var n int
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	fmt.Fscan(r, &n)
+	we := make([]int, n)
+	hi := make([]int, n)
+	gr := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &we[i], &hi[i])
+	}
+
+	for i := 0; i < n; i++ {
+		gr[i]++
+		for j := 0; j < n; j++ {
+			if we[i] > we[j] && hi[i] > hi[j] {
+				gr[j]++
+			}
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		fmt.Fprintf(w, "%d ", gr[i])
+	}
+}
