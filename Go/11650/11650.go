1e39ca81e0a9018fa7ca4905f9fdfb9507c27789
jmj005 <jmj00005@naver.com>
2022-10-18 15:24:05 +0900

    add files

--- Go/11650/11650.go
+++ Go/11650/11650.go
@@ -1,20 +1,5 @@
 package main
 
-<<<<<<< HEAD
-import "fmt"
-
-func main() {
-	var n int
-
-	fmt.Scanf("%d", &n)
-	arr := make([][2]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d %d", &arr[i][0], &arr[i][1])
-	}
-	/*for i := 0; i < n; i++ {
-		fmt.Printf("%d %d\n", arr[i][0], arr[i][1])
-	}*/
-=======
 import (
 	"bufio"
 	"fmt"
@@ -43,5 +28,4 @@ func main() {
 	for i := 0; i < n; i++ {
 		fmt.Fprintln(w, arr[i][0], arr[i][1])
 	}
->>>>>>> e29607c422b13c403c4eadafce6eaaf822511122
 }
